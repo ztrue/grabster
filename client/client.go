daf67f7f75bb7c56dbe6a6e388a3499dc78bbe75
ztrue <gztrue@gmail.com>
2016-12-21 00:55:44 +0200

    Get requests with custom timeout

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package client
 import (
   "io/ioutil"
   "net/http"
+  "time"
 )
 
 type Response struct {
@@ -12,7 +13,14 @@ type Response struct {
 }
 
 func Get(url string) (*Response, error) {
-  res, err := http.Get(url)
+  return GetWithTimeout(url, 0)
+}
+
+func GetWithTimeout(url string, timeout time.Duration) (*Response, error) {
+  client := http.Client{
+    Timeout: timeout,
+  }
+  res, err := client.Get(url)
   if err != nil {
     return &Response{0, http.Header{}, []byte{}}, err
   }
